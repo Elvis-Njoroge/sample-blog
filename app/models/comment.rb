79990eb057a957b8bb309e01bb335db5869bee13
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2023-05-11 10:11:32 +0300

    validations,relationships

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,8 @@
 class Comment < ApplicationRecord
+    belongs_to :user
+    belongs_to :blog
+
+    validates :body, presence: true
+    validates :user, presence: true
+    validates :blog, presence: true
 end
