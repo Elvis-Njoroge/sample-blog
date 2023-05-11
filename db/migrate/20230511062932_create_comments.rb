b06b207622cbd3914ab7a9c1bc54f3770d15caa0
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2023-05-11 09:51:43 +0300

    migrations

--- db/migrate/20230511062932_create_comments.rb
+++ db/migrate/20230511062932_create_comments.rb
@@ -1,6 +1,9 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
+      t.string :body
+      t.references :user, null: false, foreign_key: true
+      t.references :blog, null: false, foreign_key: true
 
       t.timestamps
     end
