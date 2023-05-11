d75eeaea13aea9b148673955a42439e74afcd94d
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2023-05-11 17:59:33 +0300

    refactor code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+    wrap_parameters format:[]
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 
     def authorized
         return render json:{error:"Not authorized!"}, status: :unauthorized
         unless session.include? :user_id
         end
+    end
+
+    def render_unprocessable_entity(invalid)
+        render json:{error:invalid.record.errors.full_messages}  ,status: :unprocessable_entity
     end
 end
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,43 +1,38 @@
 class BlogsController < ApplicationController
-    before_action :authenticate_user!, except: [:index, :show]
+    # before_action :authenticate_user!, except: [:index, :show]
     before_action :find_blog, only: [:show, :update, :destroy]
 
     # GET /blogs
     def index
-        @blogs = Blog.all
-        render json: @blogs
+        blogs = Blog.all
+        render json: blogs, except:[:created_at, :updated_at]
     end
 
     # GET /blogs/1
     def show
-        render json: @blog
+        blog = find_blog
+        render json: blog, except:[:created_at, :updated_at]
     end
 
     # POST /blogs
     def create
-        @blog = Blog.new(blog_params)
-
-        if @blog.save
-            @blog.user.update_blogs_counter
-            render json: @blog, status: :created, location: @blog
-        else
-            render json: @blog.errors, status: :unprocessable_entity
-        end
+        blog = Blog.create!(blog_params)
+        render json: blog, except:[:created_at, :updated_at]
     end
 
     # PATCH/PUT /blogs/1
     def update
-        if @blog.update(blog_params)
-        render json: @blog
-        else
-        render json: @blog.errors, status: :unprocessable_entity
-        end
+        blog = find_blog
+        blog.update(blog_params)
+        render json: blog, except:[:created_at, :updated_at], status: :ok
     end
 
     # DELETE /blogs/1
     def destroy
-        @blog.user.update_blogs_counter
-        @blog.destroy
+        blog = find_blog
+        blog.user.update_blogs_counter
+        blog.comments.destroy_all
+        blog.destroy
 
         head :no_content
 
@@ -45,14 +40,13 @@ class BlogsController < ApplicationController
 
     private
 
-
     def find_blog
-        @blog = Blog.find(params[:id])
+        blog = Blog.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+   
     def blog_params
-        params.require(:blog).permit(:title, :body, :user_id)
+        params.permit(:title, :body, :user_id)
     end
 
 end
