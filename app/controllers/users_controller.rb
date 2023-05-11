adc74c000644f8367445f4efcc72dc3c370cc6fe
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2023-05-11 17:29:56 +0300

    add cookies and sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,57 +1,59 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
     before_action :find_user, only: [:show, :update, :destroy]
+    wrap_parameters format:[]
 
   # GET /users
   def index
-    @users = User.all
-    render json: @users
+    users = User.all
+    render json: users,  only:[:name, :email]
   end
 
   # GET /users/1
-  def show
-    render json: @user
-  end
+def show
+  current_user = User.find(session[:user_id])
+  render json: current_user,  only:[:name, :email]
+end
 
   # POST /users
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+    user = User.create!(user_params)
+    render json: user, only:[:name, :email], status: :created
   end
 
   # PATCH/users/1
   def update
-    if @user.update(user_params)
-      render json: @user
+    if user.update(user_params)
+      render json: user
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: user.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /users/1
   def destroy
-    @user.destroy
+    user.destroy
   end
 
   # custom method to update blogs_counter for a user
   def update_blogs_counter
-    @user = User.find(params[:id])
-    @user.update_attribute(:blogs_counter, @user.blogs.count)
-    render json: @user
+    user = User.find(params[:id])
+    user.update_attribute(:blogs_counter, user.blogs.count)
+    render json: user,  except:[:password_digest, :created_at, :updated_at]
   end
 
   private
 
     def find_user
-      @user = User.find(params[:id])
+      user = User.find(params[:id])
     end
 
    
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.permit(:name, :email, :password)
+    end
+
+     def render_unprocessable_entity(invalid)
+        render json:{error:invalid.record.errors.full_messages}  ,status: :unprocessable_entity
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   resources :blogs
   resources :comments
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/auth",to: "users#show"
+  post "/login",to: "sessions#create"
+  delete "/logout",to: "sessions#destroy"
+
 end
