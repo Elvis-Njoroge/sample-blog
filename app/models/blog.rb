7e95c2c6a16f927880ca36ebe47c93de43f1b57d
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2023-05-11 11:21:12 +0300

    add comments_counter

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,9 @@
 class Blog < ApplicationRecord
     belongs_to :user
     has_many :comments
+
+    def update_comments_counter
+        self.comments_counter = self.comments.count
+        self.save
+    end
 end
